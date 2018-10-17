c5d32a850cba4efc3526ea16db13bb65c7d52001
jmbhughes <hughes.jmb@gmail.com>
2018-10-17 11:31:09 -0600

    created setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='flaregress',
+    version='0.0.1',
+    author='J. Marcus Hughes',
+    author_email='hughes.jmb@gmail.com',
+    packages=['flaregress'],
+    url='',
+    license='LICENSE.txt',
+    description='Software to load solar flare data and predict their future lightcurves',
+    long_description=open('README.md').read(),
+    install_requires=["numpy",
+                      "pandas"],
+    test_suite="test"
+)
