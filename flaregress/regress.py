dd18373e3e2cdce399c924e4aa09f1b338fdbf69
jmbhughes <hughes.jmb@gmail.com>
2018-12-03 12:11:39 -0700

    multiple approaches incorporated

--- flaregress/regress.py
+++ flaregress/regress.py
@@ -1,17 +1,32 @@
 from abc import ABC, abstractmethod
-from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression as skLinearRegression
 import numpy as np
-import math
+import enum
+
+
+class RegressorMode(enum.Enum):
+    """
+    The mode the regressor is running in:
+        Recursive predicts just the next step and moves forward until a sufficient future window is predicted
+        Many models trains a different model for each lag
+        Many outputs trains one model that outputs all the time lags
+    """
+    RecursiveModel = 0
+    ManyModels = 1
+    ManyOutputs = 2
 
 
 class Regressor(ABC):
     """ A generic regressor class used in forecasting"""
 
-    def __init__(self):
+    def __init__(self, mode, historic_horizon=15, future_horizon=15):
         """ initialize regressor"""
-        pass
+        self.models = []
+        self.mode = mode
+        self.historic_horizon = historic_horizon
+        self.future_horizon = future_horizon
 
-    @abstractmethod
     def fit(self, X, y):
         """
         train the regressor
@@ -20,9 +35,27 @@ class Regressor(ABC):
         :param y: set of sample outputs
         :type y: np.ndarray
         """
+        if self.mode == RegressorMode.RecursiveModel:
+            return self._fit_recursive(X, y)
+        elif self.mode == RegressorMode.ManyModels:
+            return self._fit_many_models(X, y)
+        elif self.mode == RegressorMode.ManyOutputs:
+            return self._fit_many_outputs(X, y)
+        else:
+            raise NotImplemented("You requested a mode that was not implemented. See RegressorMode enumeration.")
+
+    @abstractmethod
+    def _fit_recursive(self, X, y):
         pass
 
     @abstractmethod
+    def _fit_many_models(self, X, y):
+        pass
+
+    @abstractmethod
+    def _fit_many_outputs(self, X, y):
+        pass
+
     def predict(self, X):
         """
         predict for a sample of inputs
@@ -31,71 +64,100 @@ class Regressor(ABC):
         :return: a set out next step outputs
         :rtype: np.ndarray
         """
-        pass
+        if self.mode == RegressorMode.RecursiveModel:
+            return self._predict_recursive(X)
+        elif self.mode == RegressorMode.ManyModels:
+            return self._predict_many_models(X)
+        elif self.mode == RegressorMode.ManyOutputs:
+            return self._predict_many_outputs(X)
+        else:
+            raise NotImplemented("You requested a mode that was not implemented. See RegressorMode enumeration.")
 
-    @abstractmethod
-    def forecast(self, curve):
+    def _predict_recursive(self, X):
         """
-        forecast the entire future for one input curve
-        :param curve: time series of brightness in one passband
-        :type curve: np.ndarray
-        :return: the future multi-step forecast
-        :rtye: np.ndarray
+        Predicts using the recursive mode
+        :param X: either a single feature set or a database
+        :return: the prediction out to the window
         """
-        pass
+        if len(X.shape) == 2:  # data base
+            return np.stack([self._predict_recursive(x) for x in X])
+        else:  # single feature set
+            history = np.zeros(self.historic_horizon + self.future_horizon)
+            history[:self.historic_horizon] = X
+            for i in range(self.historic_horizon, self.historic_horizon + self.future_horizon):
+                history[i] = self.models[0].predict(history[i - self.future_horizon:i].reshape(1, -1))
+            return history[-self.future_horizon:]
+
+    def _predict_many_models(self, X):
+        """
+        Predicts using the many models approach
+        :param X: either a single feature set or a database
+        :return: the prediction out to the window
+        """
+        if len(X.shape) == 2:
+            return np.stack([self._predict_many_models(x) for x in X])
+        else:  # a single sample was passed
+            return np.concatenate([model.predict(X.reshape(1, -1)) for model in self.models])
+
+    def _predict_many_outputs(self, X):
+        """
+        Predicts using the many outputs approach
+        :param X: either a single feature set or a database of samples
+        :return: the prediction out to the window
+        """
+        return self.models[0].predict(X)
+
+    @classmethod
+    def rmse(cls, true_values, predictions):
+        """
+        Root mean squared
+        :param true_values: true light curve
+        :param predictions: predicted light curve
+        :return: root mean squared error
+        """
+        return np.sqrt(mean_squared_error(true_values, predictions))
+
+
+class BaselineRegressor(Regressor):
+    def __init__(self, mode, historic_horizon=15, future_horizon=15):
+        """
+        initialize regressor
+        :param historic_horizon: how many historical steps to use in predicting the future
+        :type historic_horizon: int
+        :param future_horizon: how many future steps to predict
+        :type future_horizon: int
+        """
+        Regressor.__init__(self, mode, historic_horizon=historic_horizon, future_horizon=future_horizon)
+
+    def _fit_recursive(self, X, y):
+        raise NotImplementedError()
+
+    def _fit_many_models(self, X, y):
+        raise NotADirectoryError()
+
+    def _fit_many_outputs(self, X, y):
+        raise NotImplementedError()
 
 
 class LinearRegressor(Regressor):
     """ A simple linear regressor"""
 
-    def __init__(self, historical_window=5):
+    def __init__(self, mode, historic_horizon=15, future_horizon=15):
         """
-        initialize regresor
-        :param historical_window: how many historical steps to use in predicting the future
-        :type historical_window: int
+        initialize regressor
+        :param historic_horizon: how many historical steps to use in predicting the future
+        :type historic_horizon: int
+        :param future_horizon: how many future steps to predict
+        :type future_horizon: int
         """
-        Regressor.__init__(self)
-        self.model = LinearRegression()
-        self.goes_k = historical_window
+        Regressor.__init__(self, mode, historic_horizon=historic_horizon, future_horizon=future_horizon)
 
-    def fit(self, X, y):
-        """
-        train the regressor
-        :param X: set of sample inputs
-        :type X: np.ndarray
-        :param y: set of sample outputs
-        :type y: np.ndarray
-        """
-        self.model = self.model.fit(X, y)
+    def _fit_recursive(self, X, y):
+        self.models.append(skLinearRegression().fit(X, y[:, 0]))
 
-    def predict(self, X):
-        """
-        predict for a sample of inputs
-        :param X: a set of inputs
-        :type X: np.ndarray
-        :return: a set out next step outputs
-        :rtype: np.ndarray
-        """
-        return self.model.predict(X)
+    def _fit_many_models(self, X, y):
+        for i in range(self.future_horizon):
+            self.models.append(skLinearRegression().fit(X, y[:, i]))
 
-    def forecast(self, curve):
-        """
-        forecast the entire future for one input curve
-        :param curve: time series of brightness in one passband
-        :type curve: np.ndarray
-        :return: the future multi-step forecast
-        :rtye: np.ndarray
-        """
-        forecast = []
-        history = curve[:self.goes_k]
-        for i in range(self.goes_k, curve.shape[0]):
-            forecast.append(self.model.predict(history.reshape((1, -1))))
-            history = np.append(history[1:], forecast[-1])
-        return np.array(forecast)
-
-    def rmse(self, true_values, predictions):
-        rmse = 0.0; n = len(true_values)
-        for i in range(n):
-            if not math.isnan(true_values[i]): rmse += (true_values[i] - predictions[i]) ** 2
-        rmse = math.sqrt(rmse/n)
-        return rmse
+    def _fit_many_outputs(self, X, y):
+        self.models.append(skLinearRegression().fit(X, y[:, :self.future_horizon]))
